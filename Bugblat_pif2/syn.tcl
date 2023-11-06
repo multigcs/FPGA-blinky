1ecc39ad9338c244ad529ce6ed15082ffb145fc9
Oliver Dippel <odippel@ypsilon.net>
2023-11-06 16:59:34 +0100

    generating diamond tcl file from makefile

--- Bugblat_pif2/syn.tcl
+++
@@ -1,8 +0,0 @@
-prj_project open rio.ldf
-prj_run Synthesis -impl build
-prj_run Translate -impl build
-prj_run Map -impl build
-prj_run PAR -impl build
-prj_run PAR -impl build -task PARTrace
-prj_run Export -impl build -task Bitgen
-prj_project close
