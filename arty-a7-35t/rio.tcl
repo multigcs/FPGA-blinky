437f444ce12fca6ae6ec6814521f6ac76fef332e
Oliver Dippel <odippel@ypsilon.net>
2023-11-06 16:30:08 +0100

    cleanup

--- arty-a7-35t/rio.tcl
+++ arty-a7-35t/rio.tcl
@@ -15,11 +15,6 @@ opt_design
 place_design
 report_clock_utilization -file $outputDir/clock_util.rpt
 
-# Optionally run optimization if there are timing violations after placement
-#if {[get_property SLACK [get_timing_paths -max_paths 1 -nworst 1 -setup]] < 0} {
-#    puts "Found setup timing violations => running physical optimization"
-#    phys_opt_design
-#}
 write_checkpoint -force $outputDir/post_place.dcp
 report_utilization -file $outputDir/post_place_util.rpt
 report_timing_summary -file $outputDir/post_place_timing_summary.rpt
