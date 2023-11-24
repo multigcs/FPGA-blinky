56031adcfa27c1bb7e6050d0e2ad1d8f4bc5783a
Oliver Dippel <o.dippel@gmx.de>
2023-11-24 14:34:48 +0100

    adding Makefile for EPM240 - MAX II CPLD

---
+++ EPM240/rio.v
@@ -0,0 +1,16 @@
+/*
+    ######### EP4CE6E22C8 #########
+*/
+
+module rio (
+        output BLINK_LED,
+        input sysclk
+    );
+
+
+    blink #(50000000) blink1 (
+        .clk (sysclk),
+        .led (BLINK_LED)
+    );
+
+endmodule
