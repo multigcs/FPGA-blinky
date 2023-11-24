5de64e4473729388094d2493493aa7b773d20bc9
Oliver Dippel <o.dippel@gmx.de>
2023-11-24 20:48:38 +0100

    fixing Bugblat_pif2

--- Bugblat_pif2/rio.v
+++ Bugblat_pif2/rio.v
@@ -3,12 +3,13 @@
 */
 
 module rio (
-        output BLINK_LED,
-        input sysclk
+        output BLINK_LED
     );
 
+	wire sysclk;
+    OSCH #(.NOM_FREQ("133.00")) rc_oscillator(.STDBY(1'b0), .OSC(sysclk));
 
-    blink #(66500000) blink1 (
+    blink #(133000000) blink1 (
         .clk (sysclk),
         .led (BLINK_LED)
     );
